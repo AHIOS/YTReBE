a8e781f1760326972e642d8837c009995f3de9b4
AHIOS <ahios.dev@gmail.com>
2018-09-14 21:47:12 +0200

    * some envvar test fro prod deply

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const express = require('@feathersjs/express');
+
+const mainApp = express().use('/api/v1', app);
+
+const server = mainApp.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -11,3 +15,8 @@ process.on('unhandledRejection', (reason, p) =>
 server.on('listening', () =>
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
 );
+
+
+
+// Now call setup on the Feathers app with the server
+app.setup(server);
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
   console.log("connection " + app.get('mongodb'));
+  console.log( !!app.get('PRODUCTION') ? "PRODUCTION" : "DEVELOPMENT");
   app.use(function(req, res, next) {
     console.log(`${req.method} ${req.originalUrl}`)
     next();
