6ac9023e0caead9ec2a42b536c6e39726b6636f6
AHIOS <ahios.dev@gmail.com>
2018-09-14 17:54:38 +0200

    * added custom logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,4 +2,9 @@
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+  app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.originalUrl}`)
+    next();
+  });
+  
 };
