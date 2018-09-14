c0ffc249ff4da08f82f46fa41d504347203e3500
AHIOS <ahios.dev@gmail.com>
2018-09-15 01:42:07 +0200

    * production config and cleanup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
-const express = require('@feathersjs/express');
 
+const express = require('@feathersjs/express');
 const mainApp = express().use('/api/v1', app);
 mainApp.use('/', express.static(app.get('public')));
-
 const server = mainApp.listen(port);
+app.setup(server);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -16,8 +16,3 @@ process.on('unhandledRejection', (reason, p) =>
 server.on('listening', () =>
   logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
 );
-
-
-
-// Now call setup on the Feathers app with the server
-app.setup(server);
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,10 +2,8 @@
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
-  console.log( !!app.get('PRODUCTION') ? app.get('PRODUCTION') : "DEVELOPMENT");
   app.use(function(req, res, next) {
     console.log(`${req.method} ${req.originalUrl}`)
     next();
   });
-  
 };
