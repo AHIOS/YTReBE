f06234ae4740f07f7a17b08a8757b04bed4a6603
AHIOS <ahios.dev@gmail.com>
2018-09-14 18:05:00 +0200

    * additional log

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,6 +2,7 @@
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+  console.log("connection " + app.get('mongodb'));
   app.use(function(req, res, next) {
     console.log(`${req.method} ${req.originalUrl}`)
     next();
