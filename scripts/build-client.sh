9cf2767859eeacd429b285b63460b53a5c4b0f09
AHIOS <ahios.dev@gmail.com>
2018-10-18 17:40:44 +0200

    * added log on build client script

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -1,9 +1,9 @@
 mkdir checkout &&  
 cd checkout && 
 git clone https://github.com/AHIOS/YTReNuxt.git && 
+cd YTReNuxt && 
 echo commit number && git rev-list HEAD | wc -l &&
 echo commit message && git log -1 --pretty=%B | cat &&
-cd YTReNuxt && 
 yarn && 
 yarn run generate && 
 mv dist/ ../../public &&
